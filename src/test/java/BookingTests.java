1b0100f180502471027e7ed3e5f689caefed68e8
CeehSoares <c.sobreiro@gmail.com>
2023-01-24 16:30:28 -0300

    New tests.

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -18,10 +18,11 @@ import static org.hamcrest.Matchers.*;
 
 public class BookingTests {
     public static Faker faker;
-    private static RequestSpecification request;
-    private static Booking booking;
-    private static BookingDates bookingDates;
     private static User user;
+    private static Booking booking;
+    private static RequestSpecification request;
+    private static BookingDates bookingDates;
+
 
 
     //Método Setup - Ocorre antes de tudo
@@ -30,18 +31,21 @@ public class BookingTests {
     public static void Setup(){
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
         faker = new Faker();
-        user = new User(faker.name().username(),
+        user = new User(
+                faker.name().username(),
+                faker.internet().password(8,10),
                 faker.name().firstName(),
                 faker.name().lastName(),
                 faker.internet().safeEmailAddress(),
-                faker.internet().password(8,10),
                 faker.phoneNumber().toString());
 
         bookingDates = new BookingDates("2022-01-02", "2022-01-03");
+
         booking = new Booking(user.getFirstName(), user.getLastName(),
                 (float)faker.number().randomDouble(2, 50, 100000),
                 true,bookingDates,
                 "");
+
         RestAssured.filters(new RequestLoggingFilter(),new ResponseLoggingFilter(), new ErrorLoggingFilter());
     }
 
@@ -62,13 +66,28 @@ public class BookingTests {
                 .extract()
                 .response();
 
-
         Assertions.assertNotNull(response);
         Assertions.assertEquals(200, response.statusCode());
     }
 
+    // auth - POST - Create Token
     @Test
-    public void  getAllBookingsByUserFirstName_BookingExists_returnOk(){
+    public void createToken_returnOK() {
+        Response response = request
+                .when()
+                .post("/auth")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .time(lessThan(2000L))
+                .extract().response();
+
+        Assertions.assertNotNull(response);
+    }
+
+    @Test
+    public void  getAllBookingsByUserFirstName_returnOk(){
         request
                 .when()
                 .queryParam("firstName", "Ceeh")
@@ -82,7 +101,7 @@ public class BookingTests {
     }
 
     @Test
-    public void  CreateBooking_WithValidData_returnOk(){
+    public void  CreateBooking_ValidData_returnOk(){
 
         Booking test = booking;
         given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
@@ -95,7 +114,7 @@ public class BookingTests {
                 .and()
                 .assertThat()
                 .statusCode(200)
-                .contentType(ContentType.JSON).and().time(lessThan(2000L));
+                .contentType(ContentType.JSON).and().time(lessThan(2006L));
 
     }
 
@@ -111,5 +130,32 @@ public class BookingTests {
 
         Assertions.assertNotNull(response);
         Assertions.assertEquals(404, response.statusCode());
+    }
+
+    @Test
+    public void updateBooking_ValidData_returnOK() {
+        given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
+                .when()
+                .body(booking)
+                .put("/booking/696")
+                .then()
+                .assertThat().statusCode(200)
+                .and()
+                .time(lessThan(2089L));
+    }
+
+    @Test
+    public void deleteBooking_returnOK() {
+        given().config(RestAssured.config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
+                .when()
+                .delete("/booking/505")
+                .then()
+                .assertThat().statusCode(201).and()
+                .statusLine(containsString("Created"))
+                .time(lessThan(2178L));
     }
 }
